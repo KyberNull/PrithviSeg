8d084f0b817d78c74b8d1e79ace9eb420d5a0efb
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-04-14 08:00:05 +0000

    fix: incomplete file name change in training module

--- training/__init__.py
+++ training/__init__.py
@@ -1,7 +1,7 @@
 """Training package for pretrain/train entrypoints and shared primitives."""
 
 from .primitives import setup_scheduler, train_batch, validate
-from .phase_io import get_pretrain_dataloaders, get_train_dataloaders, load_checkpoint_pretrain, load_checkpoint_train
+from .dataset_io import get_pretrain_dataloaders, get_train_dataloaders, load_checkpoint_pretrain, load_checkpoint_train
 
 __all__ = [
 	"train_batch",
